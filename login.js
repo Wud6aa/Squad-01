09d9b69218120ae4aa82a300b683ecf7a3fd6700
Guilherme Fontes <132164831+Wud6aa@users.noreply.github.com>
2023-10-06 17:58:54 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+function TelaCadastro(){
+    location.href = "Cadastro.html"
+}
+
+function PáginaInicial(){
+    location.href = "index.html"
+}
+
+function PáginaLogin(){
+    location.href = "pagina-login.html"
+}
