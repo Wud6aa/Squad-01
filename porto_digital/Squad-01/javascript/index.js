f13f796b942896cb63ea0806785b2ffb020a10c9
William Santana <williamandrade1058@gmail.com>
2023-10-21 18:46:55 -0300

    test in index.html and js

--- porto_digital/Squad-01/javascript/index.js
+++ porto_digital/Squad-01/javascript/index.js
@@ -1,3 +1,9 @@
-function Enviar() {
-    location.href = 'pagina-agendamento.html'
-};
+// function Enviar() {
+//     location.href = 'pagina-agendamento.html'
+// };
+
+const Enviar = () => {
+    document.getElementById('teste').addEventListener('click', function () {
+        location.reload()
+    })
+}
