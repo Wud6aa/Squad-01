58895db46903b0c66475f8d0ff68bacd4330d686
William Santana <williamandrade1058@gmail.com>
2023-11-03 18:17:51 -0300

    configurando pagina relatorio

--- porto_digital/Squad-01/javascript/pag_relatorio.js
+++ porto_digital/Squad-01/javascript/pag_relatorio.js
@@ -1,4 +1,18 @@
-const msg_confirmacao = () => {
-    let result = document.getElementById("result");
-    result.textContent = 'Agendamento confirmado!';
-};
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
